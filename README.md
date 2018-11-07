750d5dec5048a4182afd87b640dcd23245fd4f50
pa2tie <myadsagain@gmail.com>
2018-11-08 02:47:45 +0400

    refactoring main.js and resolve tasks from 1 homework

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # mercury_auth_form
 
-valid data
+How run project on Windows
+
+1. Clone this repo
+   $ git clone https://github.com/pa2tie/mercury_auth_form
+2. Go to new directory
+   $ cd mercury_auth_form
+3. Install "serve" module global
+   $ npm install -g serve
+4. And then, finally, run it using this command:
+   $ serve
+5. Click the link to open project in browser
+   You can stop serve by hitting CTRL + C
+
+
+Valid data for success login
 
 "email": "user@example.com"
-
 "password": "mercdev" 
--- js/main.js
+++ js/main.js
@@ -1,69 +1,147 @@
-window.addEventListener("load", function () {
-    function sendData(url, form, success, showError) {
-        var data = objectifyForm(form);
-        data = JSON.stringify(data);
-        console.log(data);
-        var params = {
-            headers: {
-                "Content-Type":"application/json; charset=utf-8"
-            },
-            body: data,
-            method: "POST"
-        }
-        
-        fetch(url, params)
-        .then(data => {
-            if (data.status == 200) {
-                return data.json();
-            } else {
-                throw new Error(data.json());
-            }
-        })
-        .then(res => {console.log(res);success(res);})
-        .catch(error => showError(error));
-    }
+var loginFormWrapper = document.getElementsByClassName("login-form-wrapper")[0];
+var loginForm = document.getElementById("loginForm");
+var emailInputElement = loginForm.getElementsByClassName("login__form-email")[0];
 
-    function objectifyForm(formArray) { //serialize data function
-
-        var returnArray = {};
-        for (var i = 0; i < formArray.length; i++){
-            if (formArray[i]['name']) {
-                returnArray[formArray[i]['name']] = formArray[i]['value'];
-            }
-        }
-        return returnArray;
-    }
-
-    function showProfile(profile) {
-        removeInputError();
-        document.getElementsByClassName("login__form-email")[0].value = "";
-        document.getElementsByClassName("login__form-password")[0].value = "";
-        document.getElementsByClassName("error-msg")[0].style.display = "none";
-        document.getElementsByClassName("profile-img")[0].src = profile["photoUrl"];
-        document.getElementsByClassName("profile-name")[0].textContent = profile["name"];
-        document.getElementsByClassName("login-form")[0].style.display = "none";
-        document.getElementsByClassName("profile")[0].style.display = "flex";
-    }
-
-    function showError(error) {
-        document.getElementsByClassName("error-msg")[0].style.display = "block";
-        document.getElementsByClassName("login__form-email")[0].classList.add("error-input");
-    }
-
-    var loginForm = document.getElementById("loginForm");
-
-    loginForm.addEventListener("submit", function (event) {
-        const url = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
-        event.preventDefault();
-        sendData(url, loginForm, showProfile, showError);
-    })
+emailInputElement.addEventListener("input", async (event) => {
+    await removeInputError(emailInputElement);
 });
 
-function removeInputError() {
-    document.getElementsByClassName("login__form-email")[0].classList.remove("error-input");
+loginForm.addEventListener("submit", async (event) => {
+    await event.preventDefault();
+    
+    await disableFormInputs(loginForm, true);
+
+    var loginResponse = await loginRequest(loginForm);
+    var loginResponseData = await loginResponse.json();
+
+    if (loginResponse.status == 200) {
+        await removeInputError(emailInputElement);
+        await clearFormInputs(loginForm);
+        await removeFormError(loginForm);
+        await hideElement(loginFormWrapper);
+        await setProfile(loginResponseData);
+    } else {
+        switch(loginResponse.status) {
+            case 400:
+                await removeFormError(loginForm);
+                await setFormError(loginForm, loginResponseData);
+                await setInputError(emailInputElement);
+                break;
+            default:
+                await removeFormError(loginForm);
+                await setFormError(loginForm, loginResponseData);
+                break;
+        }
+    }
+
+    await disableFormInputs(loginForm, false);
+});
+
+async function loginRequest(form) {
+    const url = 'https://us-central1-mercdev-academy.cloudfunctions.net/login';
+    var data = await objectifyForm(form);
+    data = await JSON.stringify(data);
+
+    var params = {
+        headers: {
+            "Content-Type":"application/json; charset=utf-8"
+        },
+        body: data,
+        method: "POST"
+    }
+    
+    return await fetch(url, params);
 }
 
-function logout() {
-    document.getElementsByClassName("profile")[0].style.display = "none";
-    document.getElementsByClassName("login-form")[0].style.display = "block";
+async function objectifyForm(formArray) { //serialize form data function
+    var returnArray = {};
+    Array.prototype.slice.call(formArray).forEach(el => {
+        if (el['name']) returnArray[el['name']] = el['value']; 
+    });
+    return returnArray;
+}
+
+async function disableFormInputs(form, state) {
+    Array.prototype.slice.call(form.elements).forEach(el => {
+        el.readOnly = state;
+    });
+}
+
+async function createProfileElement(profileData) {
+    var profile = await document.createElement("div");
+    await profile.classList.add("profile");
+
+    var profileImg = await document.createElement("img");
+    profileImg.src = await profileData["photoUrl"];
+    await profileImg.classList.add("profile-img");
+    await profile.appendChild(profileImg);
+
+    var profileName = await document.createElement("span");
+    profileName.textContent = await profileData["name"];
+    await profileName.classList.add("profile-name");
+    await profile.appendChild(profileName);
+
+    var logoutBtn = await document.createElement("button");
+    await logoutBtn.classList.add("btn");
+    await logoutBtn.classList.add("logout-btn");
+    logoutBtn.innerHTML = "Logout";
+    logoutBtn.addEventListener("click", async (event) => {
+        await logout(profile);
+    });
+    await profile.appendChild(logoutBtn);
+
+    return profile;
+}
+
+async function setProfile(profileData) {
+    var el = await createProfileElement(profileData);
+    await document.getElementsByClassName("login")[0].appendChild(el);
+}
+
+async function removeProfile(profileElement) {
+    if (profileElement) profileElement.remove();
+}
+
+async function showElement(form) {
+    form.style.display = "block";
+}
+
+async function hideElement(form) {
+    form.style.display = "none";
+}
+
+async function setFormError(form, error) {
+    var errMsg = await createErrorMsgElement(error);
+    await form.insertBefore(errMsg, form.getElementsByClassName("btn")[0]);
+}
+
+async function removeFormError(form) {
+    var errorMsgElement = form.getElementsByClassName("error-msg")[0];
+    if (errorMsgElement) errorMsgElement.remove();
+}
+
+async function createErrorMsgElement(error) {
+    var p = document.createElement("p");
+    p.innerHTML = error["error"];
+    p.classList.add("error-msg");
+    return p;
+}
+
+async function setInputError(inputElement) {
+    if (inputElement) inputElement.classList.add("error-input");
+}
+
+async function removeInputError(inputElement) {
+    if (inputElement) inputElement.classList.remove("error-input"); 
+}
+
+async function clearFormInputs(form) {
+    Array.prototype.slice.call(form.elements).forEach(el => {
+        el.value = "";
+    });
+}
+
+async function logout(profileElement) {
+    removeProfile(profileElement);
+    loginFormWrapper.style.display = "block";
 }
